3da6e126b80164d00363fc2462d74cd3516eb20f
William Oquendo <wfoquendop@unal.edu.co>
2020-09-09 12:54:27 -0500

    Added latest Makefile version

--- 2020-09-09-Makefiles/plot.py
+++ 2020-09-09-Makefiles/plot.py
@@ -6,7 +6,8 @@ datax, datay = np.loadtxt("datos.txt", unpack = True)
 fig, ax = plt.subplots()
 ax.plot(datax, datay, 'r-o', label="Data")
 ax.legend()
-ax.set_xlabel("xtitle")
+ax.set_xlabel("Numero de terminos")
+ax.set_ylabel("Delta %")
 
 
 fig.savefig("fig.pdf")
